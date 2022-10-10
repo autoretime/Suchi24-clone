0623a20d19d0ed2b3e3df9fd9280db094aa113f1
autoretime <artlevchenko2gmail.com>
2022-10-10 17:43:06 +0300

    dz

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+
+        Category::create([
+            'name' => 'Sport',
+        ]);
+        Category::create([
+            'name' => 'Culture',
+        ]);
+
+        Product::create([
+            'name' => 'Ball',
+            'description' => 'good ',
+            'price' => '126',
+        ]);
+        Product::create([
+            'name' => 'Toy',
+            'description' => 'ball ',
+            'price' => '300',
+        ]);
+    }
+}
